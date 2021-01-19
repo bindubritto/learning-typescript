55c2b157820166d3a069ce22ea7f75035779a196
sabbir-bs <sabbir.ah@brainstation23.com>
2021-01-19 22:45:30 +0600

    learn function type

--- output/script.js
+++ output/script.js
@@ -1,13 +1,20 @@
 "use strict";
-var playerName = 'Mash';
-console.log(typeof (playerName));
-var fruits = ['apple', 'mango', 34, true];
-fruits.push(false);
-var player = {
-    name: 'sakib',
-    age: 30,
-    isCaptain: false
-};
-player.age = 40;
-// player.country = 'Bangladesh';
-console.log(player);
+// 01 - Union Types
+// let playerName = 'Mash';
+// console.log(typeof (playerName));
+// let fruits = ['apple', 'mango', 34, true];
+// fruits.push(false);
+// let player = {
+//     name: 'sakib',
+//     age: 30,
+//     isCaptain: false
+// }
+// player.age = 40;
+// // player.country = 'Bangladesh';
+// console.log(player)
+// 02 - Function type
+// const myFunc = (a: number, b: number) => {
+//     console.log(a + b);
+//     return a + b;
+// };
+// myFunc(4, 5);
--- src/script.ts
+++ src/script.ts
@@ -1,16 +1,28 @@
-let playerName = 'Mash';
-console.log(typeof (playerName));
+// 01 - Union Types
 
-let fruits = ['apple', 'mango', 34, true];
+// let playerName = 'Mash';
+// console.log(typeof (playerName));
 
-fruits.push(false);
+// let fruits = ['apple', 'mango', 34, true];
 
-let player = {
-    name: 'sakib',
-    age: 30,
-    isCaptain: false
-}
+// fruits.push(false);
 
-player.age = 40;
-// player.country = 'Bangladesh';
-console.log(player)
+// let player = {
+//     name: 'sakib',
+//     age: 30,
+//     isCaptain: false
+// }
+
+// player.age = 40;
+// // player.country = 'Bangladesh';
+// console.log(player)
+
+
+// 02 - Function type
+
+// const myFunc = (a: number, b: number) => {
+//     console.log(a + b);
+//     return a + b;
+// };
+
+// myFunc(4, 5);
