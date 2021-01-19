ced0378ae73fa0b369b9e8d28933d95ac5a3b016
sabbir-bs <sabbir.ah@brainstation23.com>
2021-01-19 21:21:08 +0600

    Learn union types

--- output/script.js
+++ output/script.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('Hello world');
+var playerName = 'Mash';
+console.log(typeof (playerName));
+var fruits = ['apple', 'mango', 34, true];
+fruits.push(false);
+var player = {
+    name: 'sakib',
+    age: 30,
+    isCaptain: false
+};
+player.age = 40;
+// player.country = 'Bangladesh';
+console.log(player);
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,16 @@
-console.log('Hello world');
+let playerName = 'Mash';
+console.log(typeof (playerName));
+
+let fruits = ['apple', 'mango', 34, true];
+
+fruits.push(false);
+
+let player = {
+    name: 'sakib',
+    age: 30,
+    isCaptain: false
+}
+
+player.age = 40;
+// player.country = 'Bangladesh';
+console.log(player)
